1be642416f5001135faec4a0bdc9acc94069a039
Shaira <sil21019@byui.edu>
2025-01-17 11:42:57 +0000

    project init with nodejs and mongodb connection established

--- src/App.js
+++ src/App.js
@@ -22,19 +22,57 @@ async refreshWatchlist(){
   })
 }
 
+async addClick(){
+  var newDrama=document.getElementById("title").value;
+  const data=new FormData();
+  data.append("newDrama",newDrama);
+
+  fetch(this.API_URL+"api/watchlist/AddDrama",{
+    method:"POST",
+    body:data
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshWatchlist();
+  })
+}
+
+async deleteClick(id){
+  fetch(this.API_URL+"api/watchlist/DeleteDrama?id="+id,{
+    method:"DELETE",
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshWatchlist();
+  })
+}
+
+
 render(){
   const{dramalist}=this.state;
   return (
+    <>
     <div className="App">
     <h2>Watchlist App</h2>
 
-    <input id="newDrama" />&nbsp;
+    {/* <form id="newDrama">
+      Title: <input id="title" />
+      Year: <input id="year" />
+      Type: <input id="type" />
+      Season: <input id="season" />
+      Episodes: <input id="episodes" />
+      Status: <input id="status" />
+      Rating: <input id="rating" />
+      <button onClick={()=>this.addClick()}>Add Drama</button>
+      </form> */}
+
+    <input id="title" />
     <button onClick={()=>this.addClick()}>Add Drama</button>
       
     {dramalist.map(drama=>
-      <p>
+      <section>
         <ul>
-          <li key="id">
+          <li key={drama.id}>
           <b>{drama.title}</b><br/>
           Year: {drama.year}<br/>
           Type: {drama.type}<br/>
@@ -44,12 +82,11 @@ render(){
           Rating: {drama.rating}<br/>
           </li>
         </ul>
-       
-        
-    
-      </p>
+       <button onClick={()=>this.deleteClick(drama.id)}>Delete Drama</button>
+      </section>
     )}
     </div>
+    </>
   );
 }
 }
